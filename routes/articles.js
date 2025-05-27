1f81ab7d3cffc967bde56f7eedc5304a6fe51cee
윤희원 <>
2025-05-27 15:24:28 +0900

    add articles API & edit products API

--- routes/articles.js
+++ routes/articles.js
@@ -4,34 +4,34 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 var router = express.Router();
 const express = require('express');
-const { CreateProduct, PatchProduct } = require('../dtos/products.dto');
+const { CreateArticle, PatchArticle } = require('../dtos/articles.dto');
 
-// 제품 ID를 파라미터로 받아 해당 제품의 상세 정보를 조회하는 API
+// 게시글 ID를 파라미터로 받아 해당 게시글의 상세 정보를 조회하는 API
 router.get('/:id', async (req, res, next) => {
     try {
         const id = Number(req.params.id);
-        const product = await prisma.product.findUnique({
+        const article = await prisma.article.findUnique({
             where: { id },
-            select: { id: true, name: true, description: true, price: true, tags: true, createdAt: true }
+            select: { id: true, title: true, content: true, createdAt: true }
         });
-        if (!product) {
-            const error = new Error('Cannot find given product');
+        if (!article) {
+            const error = new Error('Cannot find given article');
             error.statusCode = 404;
             return next(error)
         }
-        res.status(200).json(product);
+        res.status(200).json(article);
     } catch (error) {
         next(error);
     }
 });
 
 
-/*name, description 필터링 기능을 포함한 제품 목록 조회 API,
+/*title, content 필터링 기능을 포함한 제품 목록 조회 API,
 offset 방식의 페이지네이션, 
 최신순으로 정렬 기능*/
 router.get('/list', async (req, res, next) => {
     let orderBy;
-    const { offset = 0, limit = 10, order = 'newest', name, description } = req.query;
+    const { offset = 0, limit = 10, order = 'newest', title, content } = req.query;
     switch (order) {
         case 'oldest':
             orderBy = { createdAt: 'asc' };
@@ -41,65 +41,64 @@ router.get('/list', async (req, res, next) => {
             orderBy = { createdAt: 'desc' };
     };
 
-    const where = name || description ? {
+    const where = title || content ? {
         OR: [
-            { name: { contains: name || '', mode: 'insensitive' } },
-            { description: { contains: description || '', mode: 'insensitive' } }
+            { title: { contains: title || '', mode: 'insensitive' } },
+            { content: { contains: content || '', mode: 'insensitive' } }
         ]
     } : {};
 
     try {
-        const products = await prisma.product.findMany({
+        const articles = await prisma.article.findMany({
             where,
             orderBy,
             skip: parseInt(offset),
             take: parseInt(limit),
-            select: { id: true, name: true, price: true, createdAt: true }
+            select: { id: true, title: true, content: true, createdAt: true }
         });
-        res.status(200).send(products);
+        res.status(200).send(articles);
     } catch (error) {
         next(error);
     }
 });
 
 
-// 상품 등록 API
+//게시글 등록 API
 //추후에 이미지 파일 등록까지 추가할 예정
-// userId 고민해보기 
 router.post('/create', async (req, res, next) => {
     try {
-        assert(req.body, CreateProduct);
-        const { name, description, price, tags } = req.body;
+        assert(req.body, CreateArticle);
+        const { title, content } = req.body;
         const userId = req.user.id;
 
-        const product = await prisma.$transaction(async (tx) => {
-            const product = await tx.product.create({
-                data: { name, description, price, tags, userId },
+        const article = await prisma.$transaction(async (tx) => {
+            const article = await tx.article.create({
+                data: { title, content, userId },
             });
-            return product;
+            return article;
         });
-        res.status(201).send({ id: product.id });
+        res.status(201).send({ id: article.id });
     } catch (error) {
         next(error);
     }
 });
 
-// 상품 수정 API
+// 게시글 수정 API
 router.patch('/:id', async (req, res, next) => {
     try {
-        assert(req.body, PatchProduct);
+        assert(req.body, PatchArticle);
         const id = Number(req.params.id);
-        const product = await prisma.product.update({
+        const article = await prisma.article.update({
             where: { id },
             data: req.body,
         });
-        res.status(200).send(product);
+        res.status(200).send(article);
     } catch (error) {
         next(error);
     }
 });
 
-// 상품 삭제 API
+// 게시글 삭제 API
 router.delete('/:id', async (req, res, next) => {
     try {
         const id = Number(req.params.id);
