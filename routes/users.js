0a0d1b85f07a942085911a2890869a2b9cb22c6b
윤희원 <>
2025-05-27 15:23:32 +0900

    add users API

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,54 @@
 var express = require('express');
 var router = express.Router();
+const { CreateUser } = require('../dtos/users.dto');
+const { assert } = require('superstruct');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const app = require('../app');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+// user list 조회
+router.get('/list', async (req, res, next) => {
+  try {
+    const { offset = 0, limit = 10 } = req.query;
+    const users = await prisma.user.findMany({
+      skip: parseInt(offset),
+      take: parseInt(limit),
+      select: { id: true, firstName: true, lastName: true, email: true, createdAt: true }
+    });
+    res.status(200).send(users);
+  } catch (error) {
+    next(error);
+  }
+}
+);
+
+// user 생성
+router.post('/create', async (req, res, next) => {
+  try {
+    assert(req.body, CreateUser);
+    const { firstName, lastName, email } = req.body;
+
+    const user = await prisma.user.create({
+      data: { firstName, lastName, email },
+    })
+    res.status(201).send({ id: user.id });
+
+  } catch (error) {
+    next(error);
+  }
 });
+
+//user 삭제 
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const id = Number(req.params.id);
+    await prisma.user.delete({
+      where: { id },
+    })
+  } catch (error) {
+    next(error);
+  }
+})
 
 module.exports = router;
