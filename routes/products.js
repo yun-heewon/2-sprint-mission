2c8daf53174b76dd41e0517a34c006514f67c4be
윤희원 <>
2025-05-27 11:26:57 +0900

    product API 생성성

---
+++ routes/products.js
@@ -0,0 +1,116 @@
+const { assert } = require("superstruct");
+const app = require("../app");
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const { CreateProduct, PatchProduct } = require('../dtos/products.dto');
+var router = express.Router();
+const express = require('express');
+
+
+// 제품 ID를 파라미터로 받아 해당 제품의 상세 정보를 조회하는 API
+router.get('/:id', async (req, res, next) => {
+    try {
+        const { id } = Number(req.params.id);
+        const product = await prisma.product.findUnique({
+            where: { id: parseInt(id) },
+            select: { name: true, description: true, price: true, tags: true, createdAt: true }
+        });
+        if (!product) {
+            const error = new Error('Cannot find given product');
+            error.statusCode = 404;
+            return next(error)
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+/*name, description 필터링 기능을 포함한 제품 목록 조회 API,
+offset 방식의 페이지네이션, 
+최신순으로 정렬 기능*/
+router.get('/list', async (req, res, next) => {
+    let orderBy;
+    const { offset = 0, limit = 10, order = 'newest', name, description } = req.query;
+    switch (order) {
+        case 'oldest':
+            orderBy = { createdAt: 'asc' };
+            break;
+        case 'newest':
+        default:
+            orderBy = { createdAt: 'desc' };
+    };
+
+    const where = name || description ? {
+        OR: [
+            { name: { contains: name || '', mode: 'insensitive' } },
+            { description: { contains: description || '', mode: 'insensitive' } }
+        ]
+    } : {};
+
+    try {
+        const products = await prisma.product.findMany({
+            where,
+            orderBy,
+            skip: parseInt(offset),
+            take: parseInt(limit),
+            select: { id: true, name: true, price: true, createdAt: true }
+        });
+        res.status(200).send(products);
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+// 상품 등록 API
+//추후에 이미지 파일 등록까지 추가할 예정
+router.post('/create', async (req, res, next) => {
+    try {
+        assert(req.body, CreateProduct);
+
+        const { name, description, price, tags } = req.body;
+
+
+        const product = await prisma.$transaction(async (tx) => {
+            const product = await tx.product.create({
+                data: { name, description, price, tags },
+            });
+            return product;
+        });
+        res.status(201).send({ id: product.id });
+    } catch (error) {
+        next(error);
+    }
+});
+
+// 상품 수정 API
+router.patch('/:id', async (req, res, next) => {
+    assert(req.body, PatchProduct);
+    try {
+        const { id } = Number(req.params.id);
+        const product = await prisma.product.update({
+            where: { id },
+            data: req.body,
+        });
+        res.status(200).send(product);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// 상품 삭제 API
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const { id } = Number(req.params.id);
+        const product = await prisma.product.delete({
+            where: { id },
+        })
+    } catch (error) {
+        next(error);
+    }
+})
+
+
+module.exports = router;
