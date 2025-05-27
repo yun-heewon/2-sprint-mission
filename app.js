0d5ca4309149015f490e1d4e33db5c67d1ff07e6
윤희원 <>
2025-05-27 15:22:04 +0900

    add router

--- app.js
+++ app.js
@@ -8,12 +8,11 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productsRouter = require('./routes/products');
+var articlesRouter = require('./routes/articles');
 
 var app = express();
 
-
 // view engine setup
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
+app.use('/articles', articlesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
