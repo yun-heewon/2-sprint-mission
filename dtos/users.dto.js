466de1529653658ece58332ef4efd34e3a1a8466
yun-heewon <union00916@gmail.com>
2025-06-25 15:16:18 +0900

    user 스키마 변경, dto 모델 변경

--- dtos/users.dto.js
+++ dtos/users.dto.js
@@ -1,12 +1,13 @@
-const { object, string, size, partial, define } = require('superstruct');
+const { object, string, size, partial, define, optional } = require('superstruct');
 const isEmail = require('is-email');
 
 var Email = define('Email', isEmail);
 
 const CreateUser = object({
-    firstName: size(string(), 1, 30),
-    lastName: size(string(), 1, 30),
     email: Email,
+    nickname: size(string(), 1, 15),
+    password: string(),
+    image: optional(string())
 });
 
 const PatchUser = partial(CreateUser);
