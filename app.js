91f59baa4f5f8ab849d486096b2275ae7aa505c8
yun-heewon <union00916@gmail.com>
2025-06-26 10:35:08 +0900

    app.js  passport 추가

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var app = express();
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
+app.use(passport.initialize());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
