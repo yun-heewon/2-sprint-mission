d3e6aa5edd3acffd419a1ce982565a22d82fe98b
yun-heewon <union00916@gmail.com>
2025-06-26 10:31:37 +0900

    dto에서 userId 삭제

--- dtos/articles.dto.js
+++ dtos/articles.dto.js
@@ -3,7 +3,6 @@ const { object, string, partial, number } = require("superstruct");
 const CreateArticle = object({
     title: string(),
     content: string(),
-    userId: number()
 });
 
 const PatchArticle = partial(CreateArticle);
