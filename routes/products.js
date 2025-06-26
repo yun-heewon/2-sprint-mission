9789e6507cb4c4c1b2fe2571bb4caa4368a2195a
yun-heewon <union00916@gmail.com>
2025-06-26 13:13:08 +0900

    product 생성, 수정, 삭제 구현

--- routes/products.js
+++ routes/products.js
@@ -5,7 +5,95 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const { CreateProduct, PatchProduct } = require('../dtos/products.dto');
 
+router.post('/create', passport.authenticate('access-token', { session: false }), createProduct);
+router.patch('/update/:id', passport.authenticate('access-token', { session: false }), updateProduct);
+router.delete('/:id', passport.authenticate('access-token', { session: false }), deleteProduct);
 
+//로그인한 사용자의 상품 등록
+async function createProduct(req, res, next) {
+    try {
+        assert(req.body, CreateProduct);
+    } catch (error) {
+        console.error(error);
+        return res.status(400).json({ message: 'Invalid Product data', errors: error.message });
+    }
+
+    const { name, description, price, tags } = req.body;
+    const user = req.user;
+
+    try {
+        const product = await prisma.product.create({
+            data: { name, description, price, tags, userId: user.id },
+            select: { name: true, description: true, price: true, tags: true, createdAt: true }
+        });
+        res.status(201).json(product);
+    } catch (error) {
+        console.error('Failed to create product:', error);
+        next(error);
+    }
+}
+
+//로그인한 사용자의 상품 수정
+async function updateProduct(req, res, next) {
+    try {
+        assert(req.body, PatchProduct);
+    } catch (error) {
+        console.error(error);
+        return res.status(400).json({ message: 'Invalid Product data', errors: error.message });
+    }
+
+    const { id } = req.params;
+    const user = req.user;
+
+    try {
+        const product = await prisma.product.findUnique({
+            where: { id: Number(id) }
+        });
+
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+
+        if (product.userId !== user.id) {
+            return res.status(403).json({ message: 'You are not authorized to update this product.' });
+        }
+
+        const updatedProduct = await prisma.product.update({
+            where: { id: Number(id) },
+            data: req.body,
+            select: { name: true, description: true, price: true, tags: true, updatedAt: true }
+        })
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        console.error('Failed to update product:', error);
+        next(error);
+    }
+}
+
+//로그인한 사용자의 상품 삭제
+async function deleteProduct(req, res, next) {
+    try {
+        const { id } = req.params;
+        const user = req.user;
+
+        const product = await prisma.product.findUnique({ where: { id: Number(id) } });
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+
+        if (product.userId !== user.id) {
+            return res.status(403).json({ message: 'You are not authorized to delete this product.' });
+        }
+
+        await prisma.product.delete({
+            where: { id: Number(id) },
+        });
+        res.status(204).send();
+    } catch (error) {
+        console.error('Failed to delete product:', error);
+        next(error);
+    }
+}
 
 /*name, description 필터링 기능을 포함한 제품 목록 조회 API,
 offset 방식의 페이지네이션, 
@@ -68,64 +156,5 @@ router.get('/:id', async (req, res, next) => {
         next(error);
     }
 });
-
-
-router.post('/create', async (req, res, next) => {
-    try {
-        assert(req.body, CreateProduct);
-        const { name, description, price, tags, userId } = req.body;
-
-        // 사용자 존재 확인
-        const user = await prisma.user.findUnique({
-            where: { id: userId },
-        });
-        if (!user) {
-            return res.status(404).json({ error: 'User not found' });
-        }
-
-        // 제품 생성
-        const product = await prisma.$transaction(async (tx) => {
-            const product = await tx.product.create({
-                data: { name, description, price, tags, userId },
-            });
-            return product;
-        });
-        res.status(201).json({ id: product.id });
-    } catch (error) {
-        console.error('Error creating product:', error);
-        next(error);
-    }
-});
-
-// 상품 수정 API
-router.patch('/:id', async (req, res, next) => {
-    try {
-        assert(req.body, PatchProduct);
-        const id = Number(req.params.id);
-        const product = await prisma.product.update({
-            where: { id },
-            data: req.body,
-        });
-        res.status(200).json(product);
-    } catch (error) {
-        console.error('Error updating product:', error);
-        next(error);
-    }
-});
-
-// 상품 삭제 API
-router.delete('/:id', async (req, res, next) => {
-    try {
-        const id = Number(req.params.id);
-        await prisma.product.delete({
-            where: { id },
-        })
-        res.status(204).json();
-    } catch (error) {
-        console.error('Error deleting product:', error);
-        next(error);
-    }
-})
-
 
 module.exports = router;
