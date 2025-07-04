ae7645f85bcb43982bc3753463e31379313ddcfc
yun-heewon <union00916@gmail.com>
2025-07-04 14:15:01 +0900

    라우터 index.ts로 분리

--- src/app.ts
+++ src/app.ts
@@ -5,21 +5,15 @@ import logger from 'morgan';
 import cors from 'cors';
 import { NextFunction, Request, Response } from 'express';
 import passport from './lib/passport/index';
+import { HttpError } from 'http-errors';
 
 import indexRouter from './routes/index';
-import usersRouter from './routes/users';
-import productsRouter from './routes/products';
-import articlesRouter from './routes/articles';
-import articleCommentsRouter from './routes/articleComments';
-import productCommentsRouter from './routes/productComments';
-import documentsRouter from './routes/documents';
-import likeRouter from './routes/like';
 
 const app = express();
 
 app.use(cors(
   {
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:3000',
     credentials: true,
   }
 ));
@@ -30,31 +24,32 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/products', productsRouter);
-app.use('/articles', articlesRouter);
-app.use('/articles/comments', articleCommentsRouter);
-app.use('/products/comments', productCommentsRouter);
-app.use('/documents', documentsRouter);
-app.use('/likes', likeRouter);
 app.use('/files', express.static('uploads'));
 
 // catch 404 and forward to error handler
 app.use(function (req: Request, res: Response, next: NextFunction) {
-  next(createError(404));
+  next(createError(404, 'Not Found'));
 });
 
 // error handler
-app.use(function (err: Error, req: Request, res: Response, next: NextFunction) {
-  const errorResponse = {
-    message: err.message,
-    stack: req.app.get('env') === 'development' ? err.stack : undefined,
-  };
-  res.status(err.status || 500).json(errorResponse);
+app.use(function (err: HttpError, req: Request, res: Response, next: NextFunction) {
+  const isDevelopment = req.app.get('env') === 'development';
+  const statusCode = err.status || err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
 
+  console.error(err);
+
+  const errorResponse: { message: string; stack?: string } = {
+    message: message,
+  };
+
+  if (isDevelopment && err.stack) {
+    errorResponse.stack = err.stack;
+  }
+  res.status(statusCode).json(errorResponse);
 });
+
 
 app.listen(3000, () => console.log('Server Started'));
 
-module.exports = app;
-
+export default app
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,25 @@
 import express from 'express';
+
+import usersRouter from './users';
+import productsRouter from './products';
+import articlesRouter from './articles';
+import articleCommentsRouter from './articleComments';
+import productCommentsRouter from './productComments';
+import documentsRouter from './documents';
+import likeRouter from './like';
+
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
+router.use('/users', usersRouter);
+router.use('/products', productsRouter);
+router.use('/articles', articlesRouter);
+router.use('/articles/comments', articleCommentsRouter);
+router.use('/products/comments', productCommentsRouter);
+router.use('/documents', documentsRouter);
+router.use('/likes', likeRouter);
+router.use('/files', express.static('uploads'));
+
+router.get('/', (req, res) => {
   res.status(200).json({ message: 'welcome!' });
 });
 
