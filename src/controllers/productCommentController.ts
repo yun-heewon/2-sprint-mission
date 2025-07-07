2b3c994489cadae55c83571328df08cedced0d4f
yun-heewon <union00916@gmail.com>
2025-07-07 18:37:58 +0900

    product comment 리팩토링

--- src/controllers/productCommentController.ts
+++ src/controllers/productCommentController.ts
@@ -1,38 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
 import { ProductComment } from '../dtos/comments.dto';
 import { assert } from "superstruct";
-import prisma from '../lib/prisma';
+import productCommentService from '../services/productCommentService';
 
 //로그인한 사용자의 댓글 생성(product)
 export async function createProductComment(req: Request, res: Response, next: NextFunction) {
-    if (!req.user) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
     try {
+        if (!req.user) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
         assert(req.body, ProductComment);
-    } catch (error) {
-        if (error instanceof Error) {
-            console.error(error);
-            return res.status(400).json({ message: 'Invalid Comment data', errors: error.message });
-        }
-    }
 
-    const user = req.user;
-    const productId = Number(req.params.productId);
-    try {
-        const product = await prisma.product.findUnique({
-            where: { id: productId },
-        });
-        if (!product) {
-            return res.status(404).json({ message: 'Product not found' });
-        }
-
+        const user = req.user.id;
+        const productId = Number(req.params.productId);
         const { content } = req.body;
-        const comment = await prisma.productComment.create({
-            data: { content, userId: user.id, productId: product.id },
-            select: { id: true, content: true, userId: true, productId: true, createdAt: true }
-        });
-        res.status(201).json(comment);
+
+        const newProductComment = await productCommentService.createProductComment(user, productId, { content });
+        res.status(201).json(newProductComment);
     } catch (error) {
         console.error('Failed to create product comment:', error);
         next(error);
@@ -41,39 +26,20 @@ export async function createProductComment(req: Request, res: Response, next: Ne
 
 //로그인한 사용자의 댓글 수정(product)
 export async function updateProductComment(req: Request, res: Response, next: NextFunction) {
-    if (!req.user) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
     try {
+        if (!req.user) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
         assert(req.body, ProductComment);
-    } catch (error) {
-        if (error instanceof Error) {
-            console.error(error);
-            return res.status(400).json({ message: 'Invalid Comment data', errors: error.message });
-        }
-    }
 
-    const user = req.user;
-    const commentId = Number(req.params.commentId);
-    try {
-        const comment = await prisma.productComment.findUnique({
-            where: { id: commentId },
-        });
-        if (!comment) {
-            return res.status(404).json({ message: 'Product comment not found' });
-        }
-        if (comment.userId !== user.id) {
-            return res.status(403).json({ message: 'You are not authorized to update this comment.' });
-        }
-
-
+        const user = req.user.id;
+        const commentId = Number(req.params.commentId);
         const { content } = req.body;
-        const updatedComment = await prisma.productComment.update({
-            where: { id: commentId },
-            data: { content },
-            select: { id: true, content: true, userId: true, productId: true, createdAt: true }
-        });
-        res.status(200).json(updatedComment);
+
+
+        const updatedProductComment = await productCommentService.updateProductComment(user, commentId, { content });
+        res.status(200).json(updatedProductComment);
     } catch (error) {
         console.error('Failed to update product comment:', error);
         next(error);
@@ -82,25 +48,15 @@ export async function updateProductComment(req: Request, res: Response, next: Ne
 
 //로그인한 사용자의 댓글 삭제(product)
 export async function deleteProductComment(req: Request, res: Response, next: NextFunction) {
-    if (!req.user) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
     try {
+        if (!req.user) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
         const commentId = Number(req.params.commentId)
-        const user = req.user;
+        const user = req.user.id;
 
-        const comment = await prisma.productComment.findUnique({ where: { id: commentId } });
-        if (!comment) {
-            return res.status(404).json({ message: 'Comment not found' });
-        }
-
-        if (comment.userId !== user.id) {
-            return res.status(403).json({ message: 'You are not authorized to delete this product comment.' });
-        }
-
-        await prisma.productComment.delete({
-            where: { id: commentId }
-        })
+        await productCommentService.deleteProductComment(user, commentId);
         res.status(204).send();
     } catch (error) {
         console.error('Failed to delete product comment:', error);
---
+++ src/services/productCommentService.ts
@@ -0,0 +1,77 @@
+import { Prisma } from "@prisma/client";
+import userReporitory from "../repositories/userReporitory";
+import productRepository from "../repositories/productRepository";
+import productCommentRepository from "../repositories/productCommentRepository";
+
+export class ProductCommentService {
+    async createProductComment(userId: number, productId: number, commentData: { content: string }) {
+        const user = await userReporitory.findById(userId);
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        const product = await productRepository.findById(productId);
+        if (!product) {
+            throw new Error('Product not found');
+        }
+
+        if (!commentData.content || commentData.content.trim().length === 0) {
+            throw new Error('Comment content cannot be empty.');
+        }
+
+        const createData: Prisma.ProductCommentCreateInput = {
+            content: commentData.content,
+            user: {
+                connect: { id: userId }
+            },
+            product: {
+                connect: { id: productId }
+            },
+        };
+
+        const newProductComment = await productCommentRepository.create(createData);
+        return newProductComment;
+    }
+
+    async updateProductComment(userId: number, productCommentId: number, updateData: { content: string }) {
+
+        const user = await userReporitory.findById(userId);
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        const productComment = await productCommentRepository.findById(productCommentId);
+        if (!productComment) {
+            throw new Error('Product comment not found');
+        }
+
+        if (productComment.userId !== userId) {
+            throw new Error('Unauthorized to update this product comment')
+        }
+
+        const productCommentUpdateData: Prisma.ProductCommentUpdateInput = {
+            content: updateData.content,
+        };
+
+        const updateProductComment = await productCommentRepository.update(productCommentId, productCommentUpdateData);
+
+        return updateProductComment;
+    }
+
+    async deleteProductComment(userId: number, productCommentId: number) {
+        const productComment = await productCommentRepository.findById(productCommentId);
+        if (!productComment) {
+            throw new Error('Product comment not found');
+        }
+
+        if (productComment.userId !== userId) {
+            throw new Error('You are not authorized to delete this product comment.');
+        }
+
+        await productCommentRepository.delete(productCommentId);
+
+        return { message: 'Product comment deleted successfully' };
+    }
+}
+
+export default new ProductCommentService();
