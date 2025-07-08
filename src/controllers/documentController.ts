677d64a8f4441b321e1a15bcee5d97489c03a30c
yun-heewon <union00916@gmail.com>
2025-07-08 14:08:29 +0900

    document 리팩토링

--- src/controllers/documentController.ts
+++ src/controllers/documentController.ts
@@ -3,22 +3,15 @@ import prisma from '../lib/prisma';
 import path from 'path';
 import fs from 'fs';
 import { Prisma } from '@prisma/client';
+import documentService from '../services/documentService';
 
 export async function getDocument(req: Request, res: Response, next: NextFunction) {
     try {
         const id = Number(req.params.id);
-        const document = await prisma.document.findUnique({
-            where: { id },
-        });
-        if (!document) {
-            return res.status(404).json({ error: 'Document not found' });
-        }
-        const filePath = path.join(__dirname, '../', 'uploads', document.filename);
-        if (!fs.existsSync(filePath)) {
-            console.error(`File not found on disk: ${filePath}`);
-            return res.status(500).json({ error: 'File data mismatch: File not found on server.' });
-        }
-        res.download(filePath, document.filename, (err) => {
+
+        const document = await documentService.getDocument(id);
+
+        res.download(document.filePath, document.filename, (err) => {
             if (err) {
                 console.error('Error downloading file:', err);
             }
@@ -37,15 +30,15 @@ export async function createDocument(req: Request, res: Response, next: NextFunc
 
         const fileUrl = `/files/${req.file.filename}`;
 
-        const file = await prisma.document.create({
-            data: {
-                filename: req.file.filename,
-                mimetype: req.file.mimetype,
-                size: req.file.size,
-                url: fileUrl,
-            },
-        });
-        res.status(201).json({ url: fileUrl, fileId: file.id });
+        const documentData = {
+            filename: req.file.filename,
+            mimetype: req.file.mimetype,
+            size: req.file.size,
+            url: fileUrl
+        }
+
+        const file = await documentService.createDocument(documentData);
+        res.status(201).json({ url: file.url, fileId: file.id });
     }
     catch (error) {
         console.error('Error uploading file or saving document record:', error);
@@ -61,32 +54,8 @@ export async function deleteDocument(req: Request, res: Response, next: NextFunc
     try {
         const id = Number(req.params.id);
 
-        const docToDelete = await prisma.document.findUnique({
-            where: { id },
-        })
-
-        if (!docToDelete) {
-            return res.status(404).json({ error: 'Document not found' });
-        }
-
-        // 파일 시스템에서 파일 삭제
-        const filePath = path.join(__dirname, '../', 'uploads', docToDelete.filename);
-        try {
-            if (fs.existsSync(filePath)) {
-                await fs.promises.unlink(filePath);
-                console.log(`File successfully deleted from filesystem: ${filePath}`);
-            } else {
-                console.warn(`File not found on disk, skipping filesystem delete: ${filePath}`);
-            }
-        } catch (fileDeleteError) {
-            console.error(`Error deleting file from filesystem (${filePath}):`, fileDeleteError);
-            return next(fileDeleteError);
-        }
-        // 데이터베이스에서 문서 삭제
-        await prisma.document.delete({
-            where: { id },
-        });
-        res.status(204).send();
+        const deleteDocument = await documentService.deleteDocument(id);
+        res.status(200).json(deleteDocument);
     } catch (error) {
         console.error('Error deleting document:', error);
         if (error instanceof Prisma.PrismaClientKnownRequestError) {
